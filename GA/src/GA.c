89bf663140a34b0749e45ff3bf3aaa0060befa9b
Gonzalo S. Nido <insectopalo@gmail.com>
2011-05-25 15:27:27 +1200

    A simpler version, with structures substituted by vectors and matrixes

--- GA/src/GA.c
+++ GA/src/GA.c
@@ -2,7 +2,7 @@
    Very simple genetic algorithm implementation for a toy model. The organisms
    are n-bits computer words. The fitness function is the number of ones in the
    word.
-   The population is produced by initializing m words randomly. Then
+   The population is produced by initializing m words randomly. Then,
    iteratively evaluate the fitness of each vector, keep the b vectors with the
    highest values of it and apply 1-point corssover to these, producing b new
    words -the parents are chosen randomly, there's no competition-. The new b
@@ -10,7 +10,7 @@
    Break the loop when bmax generations have been run or when a word reaches a
    fitness of 1.
 
-   Author Gonzalo S Nido <insectopalo@gmail.com>
+   Author: Gonzalo S Nido <insectopalo@gmail.com>
 */
 
 #include <stdlib.h>
@@ -18,113 +18,98 @@
 
 /* 
    Definition of the constants used in the simulation.
-   - GENOME_LENGTH (n) is the length in bits of each computer word.
-   - POPULATION_SIZE (m) is the number of words in the population
+   - G_SIZE (n) is the length in bits of each computer word.
+   - P_SIZE (m) is the number of words in the population
    - BOTTLENECK (b) is the number of genomes that survive each generation giving
      rise to the progeny by crossover
-   - MAX_GENERATIONS (bmax) is the maximum number of generations in the simulation 
+   - MAX_GENERATIONS (bmax) is the maximum number of generations in the
+     simulation 
 */
-#define GENOME_LENGTH 16
-#define POPULATION_SIZE 40
+#define G_SIZE 16
+#define P_SIZE 40
 #define BOTTLENECK 20
 #define MAX_GENERATIONS 10
 
 /*
-   Type definitions for the "organism" (or word), a simple int vector of size
-   GENOME_LENGTH and "population", a vector of pointers to "organism" data
-   types
+   Function to generate a random population. It is just a random binary matrix
+   of G_SIZE x P_SIZE (rand() % 2). The matrix must be initialized beforehand,
+   and iven as the argument to the funcion
 */
 
-typedef int ORGANISM[GENOME_LENGTH];
-
-typedef struct population {
-   ORGANISM member[POPULATION_SIZE];
-} POPULATION;
-
-
-/*
-   Function to generate a random population. The values in the ORGANISM type
-   vector are binary (rand() % 2). Note that the POPULATION data type must
-   be initialized beforehand, and the argument given to the function is the
-   pointer
-*/
-
-void rand_population(POPULATION *P, int pop_size, int gen_size) {
+void rand_population(int p[P_SIZE][G_SIZE]) {
    srand( (unsigned int) time( NULL ));
-   int i=0;
-   for (i=0;i<pop_size;i++){
-      int j=0;
-      for (j=0;j<gen_size;j++){
-         (*P).member[i][j] = rand() % 2;
+   int i,j;
+   for (i=0;i<P_SIZE;i++){
+      for (j=0;j<G_SIZE;j++){
+         p[i][j] = rand() % 2;
       }
    }
 }
 
 
 /*
-   Function to calculate the fitness (between 0 and 1) of a binary vector. The
-   pointer to the binary vector is given as an argument (note that is a
-   ORGANISM data type, since it is of constant length)
+   Function to calculate the fitness (between 0 and 1) of a given binary
+   vector. The pointer to the binary vector is given as argument to the
+   function
 */
 
-float fitness(ORGANISM * Org){
+float fitness(int * genome){
    float f=0;
-   int j=0;
-   for (j=0;j<GENOME_LENGTH;j++){
-      f += (*Org)[j];
+   int j;
+   for (j=0;j<G_SIZE;j++){
+      f += genome[j];
    }
-   f/=GENOME_LENGTH;
+   f/=G_SIZE;
    return f;
 }
 
 
 /*
-   Function to print the population, one genome per line, with its associated
-   fitness
+   Function to print the population matrix with the fitness corresponding to
+   each of the words 
 */
 
-void print_population ( POPULATION * P, int pop_size, int gen_size){
-   int i=0;
-   for (i=0;i<pop_size;i++){
-      int j=0;
-      for (j=0;j<gen_size;j++){
-         printf ("%i",(*P).member[i][j]);
+void print_population (int p[P_SIZE][G_SIZE]){
+   int i,j;
+   for (i=0;i<P_SIZE;i++){
+      for (j=0;j<G_SIZE;j++){
+         printf ("%i",p[i][j]);
       }
-      printf (" f=%.4f\n",fitness(&(*P).member[i]));
+      printf (" f=%.4f\n",fitness(p[i]));
    }
 }
 
 
 /*
    Function to do a crossover between two parents generating two descendants.
-   All organism vectors must be declared beforehand and their pointers given
-   as arguments. A last argument gives the point where the crossover happens
+   The pointers to the population matrix lines corresponding to the parents and
+   "dying" words must be given as arguments, along with an integer which
+   corresponds to the point where the crossover happens
 */
 
-void crossover(ORGANISM * parent1, ORGANISM * parent2,
-               ORGANISM * offspring1, ORGANISM * offspring2,
-               int cutting_point){
+void crossover(int * parent1, int * parent2, int * offspring1,
+               int * offspring2, int c_point){
    int j;
-   for (j=0;j<cutting_point;j++){
-      (*offspring1)[j] = (*parent1)[j];
-      (*offspring2)[j] = (*parent2)[j];
+   for (j=0;j<c_point;j++){
+      offspring1[j] = parent1[j];
+      offspring2[j] = parent2[j];
    }
-   for (j=cutting_point;j<GENOME_LENGTH;j++){
-      (*offspring1)[j] = (*parent2)[j];
-      (*offspring2)[j] = (*parent1)[j];
+   for (j=c_point;j<G_SIZE;j++){
+      offspring1[j] = parent2[j];
+      offspring2[j] = parent1[j];
    }
 }
 
 
 /*
-   Functions to sort the organisms in the population according to their
-   respective fitness. The function "comp" is used internally in
-   sort_P_by_fitness, which is an implementation of the Quicksort
-   algorithm
+   Functions to sort the words in the population according to their respective
+   fitness. The function "comp" is used internally in sort_P_by_fitness, which
+   is an implementation of the Quicksort algorithm
 */   
 
 int comp(const void *a, const void *b){
-   float diff = fitness((ORGANISM *)a) - fitness((ORGANISM *)b);
+   float diff = fitness((int *)a) - fitness((int *)b);
+   printf ("%.4f vs %.4f\n",fitness((int *)a),fitness((int *)b));
    if (diff == 0){
       return 0;
    }else if (diff < 0){
@@ -134,8 +119,8 @@ int comp(const void *a, const void *b){
    }
 }
 
-void sort_P_by_fitness(POPULATION * P, int pop_size){
-   qsort((*P).member, pop_size, sizeof(ORGANISM), comp);
+void sort_P_by_fitness(int p[P_SIZE][G_SIZE]){
+   qsort(p, P_SIZE, sizeof(int [G_SIZE]), comp);
 }
 
 
@@ -156,53 +141,59 @@ void shuffle_array(int * a, int n){
 
 
 /*
-   Function to bring the POPULATION through a generation. It randomly shuffles
-   the top BOTTLENECK organisms and mates them in order to give raise to the
-   offspring. Each pair of top fitness organisms give raise to a pair of
-   descendants wich substitute the bottom BOTTLENECK organisms in the fitness-
-   sorted POPULATION. 
+   Function to bring the population through a generation cycle. The top fittest
+   words (defined by BOTTLENECK) in the population are randomly crossovered in
+   pairs in order to give raise to the progeny. The offspring will substitute
+   the bottom words in the population.
 */
 
-void next_generation (POPULATION * P){
-   int sex_candidates[BOTTLENECK];
+void next_generation (int p[P_SIZE][G_SIZE]){
+   int fittest[BOTTLENECK];
    int i;
    for (i=0;i<BOTTLENECK;i++){
-      sex_candidates[i] = i;
+      fittest[i] = i;
    }
 
-   shuffle_array(sex_candidates, BOTTLENECK);
+   shuffle_array(fittest, BOTTLENECK);
 
    for(i=0;i<BOTTLENECK/2;i++){
-      crossover(&(*P).member[sex_candidates[i*2]], &(*P).member[sex_candidates[(i*2)+1]],
-                &(*P).member[POPULATION_SIZE - i*2 -2], &(*P).member[POPULATION_SIZE - (i*2) - 1],
-                rand() % GENOME_LENGTH);
+      crossover(p[fittest[i*2]], p[fittest[(i*2)+1]], p[P_SIZE-i*2-2],
+                p[P_SIZE-(i*2)-1], rand()%G_SIZE);
    }
 }
 
+
+/*
+   MAIN
+   GA simulation
+*/
+
 int main(){
-   if (POPULATION_SIZE % 2 || BOTTLENECK % 2){
-      printf("Re-compile with even numbers for POPULATION_SIZE and BOTTLENECK in the #declares\n");
+   if (P_SIZE % 2 || BOTTLENECK % 2){
+      printf("Re-compile with even numbers for P_SIZE and BOTTLENECK in the",
+              "#declares\n");
       return 1;
    }
 
    /* Initialization */
+   int P[P_SIZE][G_SIZE];
 
-   POPULATION P;
-   rand_population(&P,POPULATION_SIZE,GENOME_LENGTH);
-   sort_P_by_fitness(&P,POPULATION_SIZE);
-   //print_population(&P,POPULATION_SIZE,GENOME_LENGTH);
+   rand_population(P);
+   print_population(P);
+   printf("--------------------------------\n");
+   sort_P_by_fitness(P);
 
    /* Run GA (Selection + Reproduction + Termination) */
 
    int g=1;
-   while (fitness(P.member) != 1 && g <= MAX_GENERATIONS){
-      printf("Best fitness: %.4f\n",fitness(P.member));
-      next_generation(&P);
-      sort_P_by_fitness(&P,POPULATION_SIZE);
+   while (fitness(P[0]) != 1 && g <= MAX_GENERATIONS){
+      printf("Best fitness: %.4f\n",fitness(P[0]));
+      next_generation(P);
+      sort_P_by_fitness(P);
       g++;
    }
 
-   print_population(&P,POPULATION_SIZE,GENOME_LENGTH);
+   print_population(P);
    printf("Generations: %i\n",g);
    //printf("--------------------------------\n");
    
